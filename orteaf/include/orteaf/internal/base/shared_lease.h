4e83aa78dbe63e50523544a84073bac87dd93281
WARE10sai <WARE10sai@icloud.com>
2025-12-09 11:30:08 +0900

    refactor: shared lease

--- orteaf/include/orteaf/internal/base/shared_lease.h
+++ orteaf/include/orteaf/internal/base/shared_lease.h
@@ -5,92 +5,6 @@
 #include <utility>
 
 namespace orteaf::internal::base {
-
-template <class HandleT, class ResourceT, class ManagerT>
-class SharedLeaseControlBlock {
-public:
-  using HandleType = HandleT;
-  using ResourceType = ResourceT;
-  using ManagerType = ManagerT;
-  // copy and move constructor is deleted because this is must be unique
-  SharedLeaseControlBlock() = default;
-  SharedLeaseControlBlock(ManagerType *manager, HandleType handle,
-                          ResourceType resource)
-      : manager_(manager), handle_(std::move(handle)), resource_(resource) {}
-  SharedLeaseControlBlock(const SharedLeaseControlBlock &) = delete;
-  SharedLeaseControlBlock &operator=(const SharedLeaseControlBlock &) = delete;
-  SharedLeaseControlBlock(SharedLeaseControlBlock &&) = delete;
-  SharedLeaseControlBlock &operator=(SharedLeaseControlBlock &&) = delete;
-
-  void incrementRefCount() {
-    ref_count_.fetch_add(1, std::memory_order_relaxed);
-  }
-  void decrementRefCount() {
-    if (ref_count_ == 1) {
-      release();
-      return;
-    }
-    ref_count_.fetch_sub(1, std::memory_order_relaxed);
-  }
-
-  decltype(auto) operator*() noexcept {
-    if constexpr (std::is_pointer_v<ResourceT>) {
-      return *resource_;
-    } else {
-      return (resource_);
-    }
-  }
-  decltype(auto) operator*() const noexcept {
-    if constexpr (std::is_pointer_v<ResourceT>) {
-      return *resource_;
-    } else {
-      return (resource_);
-    }
-  }
-
-  // Pointer access helper (returns raw pointer regardless of ResourceT being
-  // pointer or object).
-  auto pointer() noexcept {
-    if constexpr (std::is_pointer_v<ResourceT>) {
-      return resource_;
-    } else {
-      return std::addressof(resource_);
-    }
-  }
-  auto pointer() const noexcept {
-    if constexpr (std::is_pointer_v<ResourceT>) {
-      return resource_;
-    } else {
-      return std::addressof(resource_);
-    }
-  }
-
-  template <class F> decltype(auto) with_resource(F &&f) {
-    return static_cast<F &&>(f)(resource_);
-  }
-
-  template <class F> decltype(auto) with_resource(F &&f) const {
-    return static_cast<F &&>(f)(resource_);
-  }
-
-  HandleType &handle() { return handle_; }
-
-private:
-  void release() {
-    if (manager_) {
-      manager_->release(resource_);
-      ref_count_.store(0, std::memory_order_relaxed);
-      manager_ = nullptr;
-      resource_ = nullptr;
-    }
-  }
-
-  std::atomic<std::uint32_t> ref_count_{0};
-  HandleType handle_{};
-  ManagerType *manager_{nullptr};
-  ResourceType resource_{nullptr};
-};
-
 /**
  * @brief A generic shared lease implementation that manages a resource using
  * reference counting provided by the ManagerT.
@@ -105,12 +19,12 @@ private:
  * @tparam ManagerT The type of the manager that owns the resource. Must provide
  * `retain(HandleT)` and `release(HandleT)`.
  */
-template <class HandleT, class ResourceT, class ManagerT> class SharedLease {
+template <class HandleT, class ResourceT, class ManagerT> 
+class SharedLease {
 public:
   using HandleType = HandleT;
   using ResourceType = ResourceT;
   using ManagerType = ManagerT;
-  using ControlBlockType = SharedLeaseControlBlock<HandleType, ResourceType, ManagerType>;
 
   SharedLease() = default;
   SharedLease(const SharedLease &other) { copyFrom(other); }
@@ -123,63 +37,114 @@ public:
     return *this;
   }
 
-  SharedLease(SharedLease &&other) noexcept
-      : control_block_(std::exchange(other.control_block_, nullptr)) {}
+  SharedLease(SharedLease&& other) noexcept
+        : manager_(std::exchange(other.manager_, nullptr)),
+          handle_(std::move(other.handle_)),
+          resource_(std::move(other.resource_)) {}
 
   SharedLease &operator=(SharedLease &&other) noexcept {
     if (this != &other) {
       release();
-      control_block_ = std::exchange(other.control_block_, nullptr);
+      manager_ = std::exchange(other.manager_, nullptr);
+      handle_ = std::move(other.handle_);
+      resource_ = std::move(other.resource_);
     }
     return *this;
   }
 
   ~SharedLease() { release(); }
 
-  auto operator->() { return control_block_->pointer(); }
-  auto operator*() { return *control_block_; }
+  
+    auto operator->() noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return resource_;
+        } else {
+            return std::addressof(resource_);
+        }
+    }
+    auto operator->() const noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return resource_;
+        } else {
+            return std::addressof(resource_);
+        }
+    }
 
-  auto operator->() const { return control_block_->pointer(); }
-  auto operator*() const { return *control_block_; }
+    decltype(auto) operator*() noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return *resource_;
+        } else {
+            return (resource_);
+        }
+    }
+    decltype(auto) operator*() const noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return *resource_;
+        } else {
+            return (resource_);
+        }
+    }
 
-  auto pointer() { return control_block_->pointer(); }
-  auto pointer() const { return control_block_->pointer(); }
+    // Pointer access helper (returns raw pointer regardless of ResourceT being pointer or object).
+    auto pointer() noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return resource_;
+        } else {
+            return std::addressof(resource_);
+        }
+    }
+    auto pointer() const noexcept {
+        if constexpr (std::is_pointer_v<ResourceT>) {
+            return resource_;
+        } else {
+            return std::addressof(resource_);
+        }
+    }
 
-  template <class F> void with_resource(F &&f) {
-    control_block_->with_resource(f);
-  }
+    template <class F>
+    decltype(auto) with_resource(F&& f) {
+        return static_cast<F&&>(f)(resource_);
+    }
 
-  template <class F> void with_resource(F &&f) const {
-    control_block_->with_resource(f);
-  }
+    template <class F>
+    decltype(auto) with_resource(F&& f) const {
+        return static_cast<F&&>(f)(resource_);
+    }
 
-  explicit operator bool() const { return control_block_ != nullptr; }
+  explicit operator bool() const { return manager_ != nullptr; }
 
-  HandleType &handle() { return control_block_->handle(); }
+  HandleType &handle() { return handle_; }
 
   void release() {
-    if (control_block_) {
-      control_block_->decrementRefCount();
-      control_block_ = nullptr;
-    }
+    doRelease();
   }
 
 private:
   friend ManagerType;
 
-  SharedLease(ControlBlockType *control_block)
-      : control_block_(control_block) {}
+  SharedLease(ManagerType *manager, HandleType handle, ResourceType resource) 
+  : manager_(manager), handle_(handle), resource_(resource) {}
 
   void copyFrom(const SharedLease &other) {
-    if (other.control_block_) {
-      control_block_ = other.control_block_;
-      control_block_->incrementRefCount();
-    } else {
-      control_block_ = nullptr;
+    *this = other.manager_->acquire(other.handle_);
+  }
+
+  void doRelease() {
+    if (manager_) {
+      manager_->release(handle_);
+      manager_ = nullptr;
     }
   }
 
-  ControlBlockType *control_block_{nullptr};
+  void invalidate() {
+    manager_ = nullptr;
+    handle_ = HandleType{};
+    resource_ = ResourceType{};
+  }
+
+  HandleType handle_;
+  ManagerType *manager_;
+  ResourceType resource_;
 };
 
 } // namespace orteaf::internal::base
