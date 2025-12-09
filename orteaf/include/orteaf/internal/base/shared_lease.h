6090bb833ededdec3fac5501fa611afbb79ffd97
WARE10sai <WARE10sai@icloud.com>
2025-12-09 10:40:57 +0900

    refactor: shared lease

--- orteaf/include/orteaf/internal/base/shared_lease.h
+++ orteaf/include/orteaf/internal/base/shared_lease.h
@@ -6,7 +6,7 @@
 
 namespace orteaf::internal::base {
 
-template <class ResourceT, class ManagerT>
+template <class HandleT, class ResourceT, class ManagerT>
 class SharedLeaseControlBlock {
 public:
   using HandleType = HandleT;
@@ -16,7 +16,7 @@ public:
   SharedLeaseControlBlock() = default;
   SharedLeaseControlBlock(ManagerType *manager, HandleType handle,
                           ResourceType resource)
-      : manager_(manager), handle_(handle), resource_(resource) {}
+      : manager_(manager), handle_(std::move(handle)), resource_(resource) {}
   SharedLeaseControlBlock(const SharedLeaseControlBlock &) = delete;
   SharedLeaseControlBlock &operator=(const SharedLeaseControlBlock &) = delete;
   SharedLeaseControlBlock(SharedLeaseControlBlock &&) = delete;
@@ -84,6 +84,7 @@ private:
   }
 
   std::atomic<std::uint32_t> ref_count_{0};
+  HandleType handle_{};
   ManagerType *manager_{nullptr};
   ResourceType resource_{nullptr};
 };
@@ -107,7 +108,7 @@ public:
   using HandleType = HandleT;
   using ResourceType = ResourceT;
   using ManagerType = ManagerT;
-  using ControlBlockType = SharedLeaseControlBlock<ResourceType, ManagerType>;
+  using ControlBlockType = SharedLeaseControlBlock<HandleType, ResourceType, ManagerType>;
 
   SharedLease() = default;
   SharedLease(const SharedLease &other) { copyFrom(other); }
@@ -121,13 +122,11 @@ public:
   }
 
   SharedLease(SharedLease &&other) noexcept
-      : handle_(std::exchange(other.handle_, HandleType{})),
-        control_block_(std::exchange(other.control_block_, nullptr)) {}
+      : control_block_(std::exchange(other.control_block_, nullptr)) {}
 
   SharedLease &operator=(SharedLease &&other) noexcept {
     if (this != &other) {
       release();
-      handle_ = std::exchange(other.handle_, HandleType{});
       control_block_ = std::exchange(other.control_block_, nullptr);
     }
     return *this;
@@ -154,7 +153,7 @@ public:
 
   explicit operator bool() const { return control_block_ != nullptr; }
 
-  HandleType handle() const { return handle_; }
+  HandleType handle() const { return control_block_->handle_; }
 
   void release() {
     if (control_block_) {
@@ -166,20 +165,18 @@ public:
 private:
   friend ManagerType;
 
-  SharedLease(HandleType handle, ControlBlockType *control_block)
-      : handle_(std::move(handle)), control_block_(control_block) {}
+  SharedLease(ControlBlockType *control_block)
+      : control_block_(control_block) {}
 
   void copyFrom(const SharedLease &other) {
     if (other.control_block_) {
       control_block_ = other.control_block_;
-      handle_ = other.handle_;
       control_block_->incrementRefCount();
     } else {
       control_block_ = nullptr;
     }
   }
 
-  HandleType handle_{};
   ControlBlockType *control_block_{nullptr};
 };
 
