77f9c8e7f31a7d57e358c1688646a4733283ca1e
WARE10sai <WARE10sai@icloud.com>
2025-12-18 14:27:40 +0900

    command queue lease

--- orteaf/include/orteaf/internal/runtime/allocator/resource/mps/mps_resource.h
+++ orteaf/include/orteaf/internal/runtime/allocator/resource/mps/mps_resource.h
@@ -38,7 +38,7 @@ public:
   struct LaunchParams {
     ::orteaf::internal::base::DeviceHandle device_handle;
     ::orteaf::internal::runtime::mps::manager::MpsCommandQueueManager::
-        CommandQueueWeakLease command_queue;
+        CommandQueueLease command_queue;
   };
 
   static constexpr ::orteaf::internal::backend::Backend backend_type_static() {
